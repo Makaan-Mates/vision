2b9df8d51233d639b747edfd05decc5db9008cd6
shadevkumar <shadevsrk261@gmail.com>
2024-01-24 01:43:51 +0530

    Auth0 initiated

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import './styles/index.css'
+import { Auth0Provider } from '@auth0/auth0-react';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+    <Auth0Provider
+    domain="dev-87zmqyi57qb87nxe.us.auth0.com"
+    clientId="eNSsTL1hHPsEH3fpigut00sGUTEn0Pps"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
     <App />
-  </React.StrictMode>,
+  </Auth0Provider>,
 )
---
+++ src/pages/Landing.tsx
@@ -0,0 +1,30 @@
+import { useAuth0 } from '@auth0/auth0-react'
+
+const Landing = () => {
+  const { loginWithRedirect, isAuthenticated, logout } = useAuth0()
+
+  return (
+    <div>
+      <div className="text-2xl">Landing page</div>
+      {isAuthenticated ? (
+        <button
+          onClick={() =>
+            logout({ logoutParams: { returnTo: window.location.origin } })
+          }
+          className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Logout
+        </button>
+      ) : (
+        <button
+          onClick={() => loginWithRedirect({ appState: { returnTo: '/home' } })}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Login
+        </button>
+      )}
+    </div>
+  )
+}
+
+export default Landing
